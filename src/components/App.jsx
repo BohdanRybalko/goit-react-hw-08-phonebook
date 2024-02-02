589552a063deb08aca6a290ec914d60b34e29ba4
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-02-02 13:14:49 +0200

    fix app and index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Link, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, Link } from 'react-router-dom';
 import Registration from '../pages/Registration';
 import Login from '../pages/Login';
 import Contacts from '../pages/Contacts';
@@ -19,24 +19,24 @@ const App = () => {
   };
 
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/register">Реєстрація</Link>
+          </li>
+          <li>
+            <Link to="/login">Логін</Link>
+          </li>
+          {isAuthenticated && (
             <li>
-              <Link to="/register">Реєстрація</Link>
+              <Link to="/contacts">Контакти</Link>
             </li>
-            <li>
-              <Link to="/login">Логін</Link>
-            </li>
-            {isAuthenticated && (
-              <li>
-                <Link to="/contacts">Контакти</Link>
-              </li>
-            )}
-          </ul>
-          {isAuthenticated && <UserMenu onLogout={handleLogout} />}
-        </nav>
+          )}
+        </ul>
+        {isAuthenticated && <UserMenu onLogout={handleLogout} />}
+      </nav>
+      <Routes>
         <Route path="/register" element={<Registration />} />
         <Route
           path="/login"
@@ -44,13 +44,9 @@ const App = () => {
             isAuthenticated ? <Navigate to="/contacts" /> : <Login onLogin={handleLogin} />
           }
         />
-        <PrivateRoute
-          path="/contacts"
-          element={<Contacts />}
-          isAuthenticated={isAuthenticated}
-        />
-      </div>
-    </Router>
+        <PrivateRoute path="/contacts" element={<Contacts />} isAuthenticated={isAuthenticated} />
+      </Routes>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
+import { createRoot } from 'react-dom/client';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './components/App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <Provider store={store}>
-    <Router>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>
 );
