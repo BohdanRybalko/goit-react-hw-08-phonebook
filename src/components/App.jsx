e0935ec28eecb85122bd7293f8b5e1701674ef2e
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-02-03 11:05:59 +0200

    fix app and index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-import { Routes, Route, Navigate, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
 import Registration from '../pages/Registration';
 import Login from '../pages/Login';
 import Contacts from '../pages/Contacts';
@@ -19,34 +19,41 @@ const App = () => {
   };
 
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/register">Реєстрація</Link>
-          </li>
-          <li>
-            <Link to="/login">Логін</Link>
-          </li>
-          {isAuthenticated && (
+    <Router>
+      <div>
+        <nav>
+          <ul>
             <li>
-              <Link to="/contacts">Контакти</Link>
+              <Link to="/register">Реєстрація</Link>
             </li>
-          )}
-        </ul>
-        {isAuthenticated && <UserMenu onLogout={handleLogout} />}
-      </nav>
-      <Routes>
-        <Route path="/register" element={<Registration />} />
-        <Route
-          path="/login"
-          element={
-            isAuthenticated ? <Navigate to="/contacts" /> : <Login onLogin={handleLogin} />
-          }
-        />
-        <PrivateRoute path="/contacts" element={<Contacts />} isAuthenticated={isAuthenticated} />
-      </Routes>
-    </div>
+            <li>
+              <Link to="/login">Логін</Link>
+            </li>
+            {isAuthenticated && (
+              <li>
+                <Link to="/contacts">Контакти</Link>
+              </li>
+            )}
+          </ul>
+          {isAuthenticated && <UserMenu onLogout={handleLogout} />}
+        </nav>
+        <Routes>
+          <Route path="/register" element={<Registration />} />
+          <Route
+            path="/login"
+            element={
+              isAuthenticated ? <Navigate to="/contacts" /> : <Login onLogin={handleLogin} />
+            }
+          />
+          <Route
+            path="/contacts/*"
+            element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+          >
+            <Route index element={<Contacts />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './components/App';
@@ -9,7 +10,9 @@ const root = createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+        <App />
+      </BrowserRouter>
     </React.StrictMode>
   </Provider>
 );
