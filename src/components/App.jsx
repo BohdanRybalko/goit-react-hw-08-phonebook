64797de464fc4759faae0f750f5af2d166af0a8c
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-02-07 15:38:32 +0200

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,30 @@
-import React, { useState } from 'react';
-import { Routes, Route, Navigate, Link } from 'react-router-dom';
+import React  from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Registration from '../pages/Registration';
 import Login from '../pages/Login';
 import Contacts from '../pages/Contacts';
 import PrivateRoute from '../routes/PrivateRoute';
+import { RestrictedRoute } from '../routes/RestrictedRoute';
 import UserMenu from './UserMenu';
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const handleLogin = () => {
-    setIsAuthenticated(true);
-  };
-
-  const handleLogout = () => {
-    setIsAuthenticated(false);
-  };
-
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/register">Реєстрація</Link>
-          </li>
-          <li>
-            <Link to="/login">Логін</Link>
-          </li>
-          {isAuthenticated && (
-            <li>
-              <Link to="/contacts">Контакти</Link>
-            </li>
-          )}
-        </ul>
-        {isAuthenticated && <UserMenu onLogout={handleLogout} />}
-      </nav>
-
-      <Routes>
-        <Route path="/register" element={<Registration />} />
+    <Routes>
+      <Route path="/" element={<UserMenu />}>
         <Route
-          path="/login"
-          element={
-            isAuthenticated ? <Navigate to="/contacts" /> : <Login onLogin={handleLogin} />
-          }
+          path="/Registration"
+          element={<RestrictedRoute redirectTo="/" component={<Registration />} />}
+         />
+        <Route
+          path="/Contacts"
+          element={<RestrictedRoute redirectTo="/" component={<Contacts />} />}
         />
         <Route
-          path="/contacts/*"
-          element={<PrivateRoute isAuthenticated={isAuthenticated} />}
-        >
-          <Route index element={<Contacts />} />
-        </Route>
-      </Routes>
-    </div>
+          path="/Login"
+          element={<PrivateRoute redirectTo="/login" component={<Login />} />}
+        />
+      </Route>
+    </Routes>
   );
 };
 
